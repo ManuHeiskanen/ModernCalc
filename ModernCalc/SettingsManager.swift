826abb2bc5508447f58cb91acdca55b5fdf23d3a
ManuHeiskanen <manuheiskanen@proton.me>
2025-08-30 14:39:28 +0300

    Deleted files not being used.

--- ModernCalc/SettingsManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SettingsManager.swift
-//  ModernCalc
-//
-//  Created by Manu Heiskanen on 30.8.2025.
-//
-
-import Foundation
--- ModernCalc/SettingsView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SettingsView.swift
-//  ModernCalc
-//
-//  Created by Manu Heiskanen on 30.8.2025.
-//
-
-import Foundation
